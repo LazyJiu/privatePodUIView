bde0271b536cbb7238e43fd9009221099eb96401
shibin <shibin@shibindemacbook-air.local>
2016-05-18 11:44:55 +0800

    1

---
+++ privatePodUIView/privatePodUIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  privatePodUIView
+//
+//  Created by shibin on 16/5/18.
+//  Copyright © 2016年 啾三万. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
